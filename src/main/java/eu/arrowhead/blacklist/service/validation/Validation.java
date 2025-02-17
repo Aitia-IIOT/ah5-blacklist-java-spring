92291d35878d22de0fc986490914c3af568a5739
Katinka666 <jako.katinka@gmail.com>
2025-02-17 01:27:23 +0100

    Implement InternalBlacklistMqttFilter

--- src/main/java/eu/arrowhead/blacklist/service/validation/Validation.java
+++ src/main/java/eu/arrowhead/blacklist/service/validation/Validation.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
